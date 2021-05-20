874376add3c3aeef58b1c7e2adddc60335fb0005
Tamás Boruzs <joy.soft@hotmail.com>
2021-05-20 19:19:27 +0200

    Téma vezérlése hozzáadva.

---
+++ RelaxUI/Tema.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace RelaxUI
+{
+    public class Tema
+    {
+        public string Megnevezes                  { get; set; }
+        public Color TemaSzin                     { get; set; }
+        public Color AblakHatterSzin              { get; set; }
+        public Color PanelSzin                    { get; set; }
+        public Color SzovegSzin                   { get; set; }
+        public Color SzovegLetiltottSzin          { get; set; }
+        public Color VezerloHatterSzin            { get; set; }
+        public Color VezerloKeretSzin             { get; set; }
+        public Color VezerloHatterEgerFeletteSzin { get; set; }
+        public Color VezerloKeretEgerFeletteSzin  { get; set; }
+        public Color VezerloHatterAkcioSzin       { get; set; }
+        public Color VezerloKeretAkcioSzin        { get; set; }
+        public Color VezerloHatterFokuszSzin      { get; set; }
+        public Color VezerloKeretFokuszSzin       { get; set; }
+        public Color VezerloHatterLetiltottSzin   { get; set; }
+        public Color VezerloKeretLetiltottSzin    { get; set; }
+
+        public override string ToString() => Megnevezes;
+    }
+}
---
+++ RelaxUI/Temak.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Xml;
+
+namespace RelaxUI
+{
+    public sealed class Temak : INotifyPropertyChanged
+    {
+        private static readonly Temak peldany = new();
+
+        private Temak() { }
+
+        static Temak() { }
+
+        public static Temak Egyke => peldany;
+
+        public List<Tema> TemaLista { get; set; } = null;
+
+        private Tema aktivtema = null;
+        public Tema AktivTema
+        {
+            get => aktivtema;
+            set { if (aktivtema != value) { aktivtema = value; OnPropertyChanged(); } }
+        }
+
+        public void Inicializal()
+        {
+            bool fajlhiba = false;
+            TemaLista = new();
+
+            TemaLista.Add(new Tema()
+            {
+                Megnevezes = "Alapértelmezett",
+                TemaSzin                     = ColorFromARGBHexText("FF672878"),
+                AblakHatterSzin              = ColorFromARGBHexText("FF128345"),
+                PanelSzin                    = ColorFromARGBHexText("FF202020"),
+                SzovegSzin                   = ColorFromARGBHexText("FF913654"),
+                SzovegLetiltottSzin          = ColorFromARGBHexText("FF7F7F7F"),
+                VezerloHatterSzin            = ColorFromARGBHexText("FF282828"),
+                VezerloKeretSzin             = ColorFromARGBHexText("FF323232"),
+                VezerloHatterEgerFeletteSzin = ColorFromARGBHexText("FF383838"),
+                VezerloKeretEgerFeletteSzin  = ColorFromARGBHexText("FF424242"),
+                VezerloHatterAkcioSzin       = ColorFromARGBHexText("FF585858"),
+                VezerloKeretAkcioSzin        = ColorFromARGBHexText("FF626262"),
+                VezerloHatterFokuszSzin      = ColorFromARGBHexText("FF383838"),
+                VezerloKeretFokuszSzin       = ColorFromARGBHexText("FF424242"),
+                VezerloHatterLetiltottSzin   = ColorFromARGBHexText("FF181818"),
+                VezerloKeretLetiltottSzin    = ColorFromARGBHexText("FF222222")
+            });
+
+            XmlDocument xmldoc = new();
+
+            try { xmldoc.Load("temak.xml"); }
+            catch { fajlhiba = true; }
+
+            if (!fajlhiba)
+            {
+                XmlNodeList xmltemalista = xmldoc.SelectNodes("/TemaLista/Tema");
+
+                foreach (XmlNode node in xmltemalista)
+                {
+                    Tema tema = new();
+
+                    tema.Megnevezes                   = node.Attributes["Megnevezes"].Value;
+                    tema.TemaSzin                     = ColorFromARGBHexText(node["TemaSzin"                    ].Attributes["ARGB"].Value);
+                    tema.AblakHatterSzin              = ColorFromARGBHexText(node["AblakHatterSzin"             ].Attributes["ARGB"].Value);
+                    tema.PanelSzin                    = ColorFromARGBHexText(node["PanelSzin"                   ].Attributes["ARGB"].Value);
+                    tema.SzovegSzin                   = ColorFromARGBHexText(node["SzovegSzin"                  ].Attributes["ARGB"].Value);
+                    tema.SzovegLetiltottSzin          = ColorFromARGBHexText(node["SzovegLetiltottSzin"         ].Attributes["ARGB"].Value);
+                    tema.VezerloHatterSzin            = ColorFromARGBHexText(node["VezerloHatterSzin"           ].Attributes["ARGB"].Value);
+                    tema.VezerloKeretSzin             = ColorFromARGBHexText(node["VezerloKeretSzin"            ].Attributes["ARGB"].Value);
+                    tema.VezerloHatterEgerFeletteSzin = ColorFromARGBHexText(node["VezerloHatterEgerFeletteSzin"].Attributes["ARGB"].Value);
+                    tema.VezerloKeretEgerFeletteSzin  = ColorFromARGBHexText(node["VezerloKeretEgerFeletteSzin" ].Attributes["ARGB"].Value);
+                    tema.VezerloHatterAkcioSzin       = ColorFromARGBHexText(node["VezerloHatterAkcioSzin"      ].Attributes["ARGB"].Value);
+                    tema.VezerloKeretAkcioSzin        = ColorFromARGBHexText(node["VezerloKeretAkcioSzin"       ].Attributes["ARGB"].Value);
+                    tema.VezerloHatterFokuszSzin      = ColorFromARGBHexText(node["VezerloHatterFokuszSzin"     ].Attributes["ARGB"].Value);
+                    tema.VezerloKeretFokuszSzin       = ColorFromARGBHexText(node["VezerloKeretFokuszSzin"      ].Attributes["ARGB"].Value);
+                    tema.VezerloHatterLetiltottSzin   = ColorFromARGBHexText(node["VezerloHatterLetiltottSzin"  ].Attributes["ARGB"].Value);
+                    tema.VezerloKeretLetiltottSzin    = ColorFromARGBHexText(node["VezerloKeretLetiltottSzin"   ].Attributes["ARGB"].Value);
+
+                    TemaLista.Add(tema);
+                }
+            }
+
+            AktivTema = TemaLista[0];
+        }
+
+        public void Beallit(string _temamegnevezes)
+        {
+            Tema ujtema = TemaLista.SingleOrDefault(t => string.Equals(t.Megnevezes, _temamegnevezes));
+
+            if (ujtema != null)
+                this.AktivTema = ujtema;
+        }
+
+        private static Color ColorFromARGBHexText(string _argbhextext) =>
+            Color.FromArgb(Convert.ToByte(_argbhextext[0..1], 16),
+                           Convert.ToByte(_argbhextext[2..3], 16),
+                           Convert.ToByte(_argbhextext[4..5], 16),
+                           Convert.ToByte(_argbhextext[6..7], 16));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string _propnev = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(_propnev));
+    }
+}
